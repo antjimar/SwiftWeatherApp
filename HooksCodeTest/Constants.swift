0a0f23ba96a6755286b2e197d8ffb0508fde5a2f
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-12-09 20:14:00 +0100

    Create constants and clean AppDelegate

---
+++ HooksCodeTest/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  HooksCodeTest
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 09/12/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let APIBaseURL = "http://api.openweathermap.org/data"
+    static let APIVersion = "2.5"
+    static let APIKey = "91f25ad66394930224054bf4c231b5e1"
+    
+}
