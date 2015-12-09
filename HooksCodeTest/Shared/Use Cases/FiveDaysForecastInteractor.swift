b0a43241cc323e7d66252f3a079c51d41a5bdd96
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-12-09 20:41:38 +0100

    Interactiors Files

---
+++ HooksCodeTest/Shared/Use Cases/FiveDaysForecastInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  FiveDaysForecastInteractor.swift
+//  HooksCodeTest
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 09/12/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+import Foundation
+
+class FiveDaysForecastInteractor {
+    
+}
