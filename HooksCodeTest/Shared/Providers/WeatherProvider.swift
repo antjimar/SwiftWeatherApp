72c22b47a57b6d64971698c69ec47a88a878f696
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-12-10 19:57:47 +0100

    Fix data parser

--- HooksCodeTest/Shared/Providers/WeatherProvider.swift
+++ HooksCodeTest/Shared/Providers/WeatherProvider.swift
@@ -53,39 +53,44 @@ class WeatherProvider {
         let params = ["lat": "\(location.coordinate.latitude)", "lon": "\(location.coordinate.longitude)", "appid": Constants.APIKey, "units": "metric"]
         
         requestManager.GET("forecast", parameters: params, currentTask: nil, success: { (task, responseObject) in
+            let predicate = NSPredicate(format: "%K = %@", "weatherType", WeatherType.Forecast.rawValue)
+            self.removeCacheDataWithPredicate(predicate, context: self.writeManagedObjectContext)
             
             if let aResponseObject = responseObject {
-                let cityNameString = aResponseObject["city"]!["name"]! as? String
-                let latitude = aResponseObject["coord"]!["lat"]! as? NSNumber
-                let longitude = aResponseObject["coord"]!["long"]! as? NSNumber
-                let forecastDictArray = aResponseObject["list"]! as? [AnyObject]
-                
-                var forecastArray = [WeatherEntity]()
-                
-                if let aForecastDictArray = forecastDictArray {
-                    for (_, element) in aForecastDictArray.enumerate() {
-                        let weatherEntity = WeatherEntity(currentDataWithDictionary: element as! [String: AnyObject], city: cityNameString, latitude: latitude, longitude: longitude, insertIntoManagedObjectContext: self.writeManagedObjectContext)
-                        weatherEntity.weatherType = WeatherType.Forecast.rawValue
-                        forecastArray.append(weatherEntity)
-                    }
+                let city = aResponseObject["city"] as? [String: AnyObject]
+                if let aCity = city {
+                    let cityNameString = aCity["name"]! as? String
+                    let latitude = aCity["coord"]!["lat"]! as? NSNumber
+                    let longitude = aCity["coord"]!["lon"]! as? NSNumber
+                    let forecastDictArray = aResponseObject["list"]! as? [AnyObject]
                     
-                    do {
-                        try self.writeManagedObjectContext.save()
-                    } catch let error as NSError {
-                        completion(data: nil, error: error)
-                    }
+                    var forecastArray = [WeatherEntity]()
                     
-                    var mainThreadForecastArray = [WeatherEntity]()
-                    for (_, element) in forecastArray.enumerate() {
-                        mainThreadForecastArray.append(self.managedObjectContext.objectInManagedObjectContext(element))
+                    if let aForecastDictArray = forecastDictArray {
+                        for (_, element) in aForecastDictArray.enumerate() {
+                            let weatherEntity = WeatherEntity(currentDataWithDictionary: element as! [String: AnyObject], city: cityNameString, latitude: latitude, longitude: longitude, insertIntoManagedObjectContext: self.writeManagedObjectContext)
+                            weatherEntity.weatherType = WeatherType.Forecast.rawValue
+                            forecastArray.append(weatherEntity)
+                        }
+                        
+                        do {
+                            try self.writeManagedObjectContext.save()
+                        } catch let error as NSError {
+                            completion(data: nil, error: error)
+                        }
+                        
+                        var mainThreadForecastArray = [WeatherEntity]()
+                        for (_, element) in forecastArray.enumerate() {
+                            mainThreadForecastArray.append(self.managedObjectContext.objectInManagedObjectContext(element))
+                        }
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            completion(data: mainThreadForecastArray, error: nil)
+                        })
+                        
+                    } else {
+                        // TODO: Error
+                        completion(data: nil, error: NSError(domain: "", code: 1, userInfo: nil))
                     }
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        completion(data: mainThreadForecastArray, error: nil)
-                    })
-                    
-                } else {
-                    // TODO: Error
-                    completion(data: nil, error: NSError(domain: "", code: 1, userInfo: nil))
                 }
                 
             } else {
