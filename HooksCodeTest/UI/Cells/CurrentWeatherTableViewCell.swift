65521fd34cf7fea677119b0b654f9606324b5454
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-12-10 20:09:43 +0100

    Cells and viewController

---
+++ HooksCodeTest/UI/Cells/CurrentWeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrentWeatherTableViewCell.swift
+//  HooksCodeTest
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 10/12/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HooksCodeTest/UI/Cells/ForecastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ForecastTableViewCell.swift
+//  HooksCodeTest
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 10/12/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
