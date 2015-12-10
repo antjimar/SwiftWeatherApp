267825b86ee3d771df80888625ffcabc83a11db7
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-12-10 20:01:06 +0100

    Interactor to get current Weather

--- HooksCodeTest/Shared/Use Cases/CurrentWeatherInteractor.swift
+++ HooksCodeTest/Shared/Use Cases/CurrentWeatherInteractor.swift
@@ -7,7 +7,26 @@
 //
 
 import Foundation
+import MapKit
 
 class CurrentWeatherInteractor {
     
+    lazy var weatherProvider = WeatherProvider()
+    
+    func getCurrentWeatherByLocation(location: CLLocation, completion: (weatherEntity: WeatherEntity?, error: NSError?) -> Void) {
+        weatherProvider.getCurrentWeatherByLocation(location) { (data, error) in
+            if error == nil {
+                if let aData = data {
+                    let weatherEnity = aData as! WeatherEntity
+                    completion(weatherEntity: weatherEnity, error: nil)
+                } else {
+                    // TODO
+                    completion(weatherEntity: nil, error: NSError(domain: "", code: 1, userInfo: nil))
+                }
+            } else {
+                // Error
+                completion(weatherEntity: nil, error: error)
+            }
+        }
+    }
 }
